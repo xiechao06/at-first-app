e3dcd8b1e48975c0378eb21e1ae78f2729cd41fa
xiechao06 <xiechoa06@gmail.com>
2016-03-18 09:46:59 +0800

    0

--- app/src/main/java/com/xc/at_first_app/DisplayMessageActivity.kt
+++ app/src/main/java/com/xc/at_first_app/DisplayMessageActivity.kt
@@ -1,23 +1,44 @@
 package com.xc.at_first_app
 
 import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-import android.view.ViewGroup
+import android.support.v7.widget.Toolbar
+import android.view.View
+import android.widget.RelativeLayout
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_display_message.*
+import kotlinx.android.synthetic.main.content_display_message.*
 
 class DisplayMessageActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display_message)
+        setSupportActionBar(findViewById(R.id.toolbar) as Toolbar)
 
-        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
-        val message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE)
-        val textView = TextView(this)
-        textView.text = message;
-        textView.textSize = 40.0F;
 
-        (findViewById(android.R.id.content) as ViewGroup).addView(textView)
+        fab.setOnClickListener({ view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show()
+        })
+        // EQUIVALENT TO
+        //        fab.setOnClickListener(View.OnClickListener {
+        //            view ->
+        //            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show()
+        //        })
+        // OR
+                fab.setOnClickListener(object : View.OnClickListener {
+                    override fun onClick(v: View?) {
+                        Snackbar.make(v!!, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show()
+                    }
+                })
+
+        var textView = TextView(this)
+        textView.textSize = 40.0F
+        textView.text = intent.getStringExtra(MainActivity.EXTRA_MESSAGE)
+
+        content.addView(textView)
     }
 
 }
